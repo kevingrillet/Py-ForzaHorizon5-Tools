f30ef0e6d9804f6eab6351babbf97ded71c5b1b9
kevingrillet <kevin.grillet@gmail.com>
2021-11-27 20:12:01 +0100

    Update handlercv2.py
    For 1080p

--- utils/handlercv2.py
+++ utils/handlercv2.py
@@ -20,7 +20,7 @@ class HandlerCv2:
     require_new_capture = True
     target_image = None
     target_image_debug = None
-    threshold = 0.9
+    threshold = 0.8
 
     def __init__(self, show_debug_image=False, scale=1):
         self.scale = scale
